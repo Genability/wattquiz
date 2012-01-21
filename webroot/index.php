266a7d41f588533d20b975f65777e09e6405fb2c
jasonriley <jriley@genability.com>
2012-01-21 13:00:43 -0500

    added webroot and index

---
+++ webroot/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+	<title>Watt Quiz</title>
+	
+</head>
+
+<body>
+
+<h1>Watt Quiz</h1>
+
+<p>A simple social quiz, a la freerice.com, that asks you questions and educates you about your energy. Correct answers generate watts that are donated to worthly charities.</p>
+
+</body>
+</html>
