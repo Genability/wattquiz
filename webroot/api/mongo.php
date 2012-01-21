0eb693efe8e96cf1d0b707f7d699cf27f593464a
Michael Ihle <mihle@genability.com>
2012-01-21 16:51:01 -0500

    stub code for mongo update

--- webroot/api/mongo.php
+++ webroot/api/mongo.php
@@ -3,30 +3,87 @@
 // MIHLE: make sure to mongo.so in the right directory and add 'extension=mongo.so' to PHP.ini file located in /etc
 /**  phpinfo();  **/
 
-// assumes connection to local database 
-$m = new Mongo();
 
-// select a database
-$db = $m->wattquiz;
 
-// select a collection (analogous to a relational database's table)
-$collection = $db->questions;
+class wattquiz_mongo {
 
-// add a record 
-$obj = array( "title" => "Calvin and Hobbes", "author" => "Bill Watterson" );
-$collection->insert($obj);
 
-// add another record, with a different "shape"
-$obj = array( "title" => "XKCD", "online" => true );
-$collection->insert($obj);
+	/**
+	 * Save answer to Mongo
+	 */
+	function saveAnswer($params) {
 
-// find everything in the collection
-$cursor = $collection->find();
+		
+		
+	} // end of saveAnswer method
 
-// iterate through the results
-foreach ($cursor as $obj) {
-    echo $obj["title"] . "\n";
+
+	/**
+	 * get question from Mongo
+	 */
+	function getQuestion($questionId) {
+
+			$findQuestionId = '1';
+			$question = array('questionId' => $findQuestionId, 
+			    'questionText' => 'Who will win the superbowl',
+			    'questionType' => 'multi-choice',
+			    'wattValue' => 1,
+			    'broughtBy' => 'Genability',
+			    'answers' => array(
+			        array('answerId' => 'a','answerValue' => '49ers'),
+			        array('answerId' => 'b','answerValue' => '49ers'),
+			        array('answerId' => 'c','answerValue' => '49ers'),
+			        array('answerId' => 'd','answerValue' => '49ers')
+			        )
+			);
+    
+		return $question;
+
+	} // end of getQuestion method
+	
+
+	/**
+	 * Get Answers from Mongo
+	 */
+	function getAnswers($params) {
+
+		
+		
+	} // end of getAnswers method
+
+
+	function updateMongo($collectionName, $collection){
+		// assumes connection to local database 
+		$m = new Mongo();
+
+		// select a database
+		$db = $m->wattquiz;
+
+		// select a collection (analogous to a relational database's table)
+		$collection = $db->questions;
+
+		// add a record 
+		$obj = array( "title" => "Calvin and Hobbes", "author" => "Bill Watterson" );
+		$collection->insert($obj);
+
+		// add another record, with a different "shape"
+		$obj = array( "title" => "XKCD", "online" => true );
+		$collection->insert($obj);
+
+		// find everything in the collection
+		$cursor = $collection->find();
+
+		// iterate through the results
+		foreach ($cursor as $obj) {
+		    echo $obj["title"] . "\n";
+		}
+	}
+
 }
+
+
+
+
 
 
 ?>
