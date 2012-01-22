1997748bf760c5ed266d456a9e9e394f37c1c1e2
Michael Ihle <mihle@genability.com>
2012-01-22 15:10:06 -0500

    updated questions

--- webroot/setup.php
+++ webroot/setup.php
@@ -23,8 +23,43 @@ print_r($response);
 
 $collection = $db->question;
 
+
+
 $question1 = array(
 			    'questionId' => 1, 
+			    'questionText' => 'What was your total consumption last month?',
+			    'questionType' => 'multi-choice',
+			    'wattValue' => 3,
+			    'broughtBy' => 'Genability',
+			    'answerTip' => 'Consumption rises in the winter months',			    
+			    'answers' => array(
+			        array('answerId' => 'a','answerValue' => '0-50 kWh', 'answerRank' => 1),
+			        array('answerId' => 'b','answerValue' => '50-200 kWh', 'answerRank' => 2),
+			        array('answerId' => 'c','answerValue' => '200-500 kWh', 'answerRank' => 3),
+			        array('answerId' => 'd','answerValue' => '500-1000 kWh', 'answerRank' => 4)
+			        )
+			);
+
+
+$question2 = array(
+			    'questionId' => 2, 
+			    'questionText' => 'What was your monthly electricity bill last month?',
+			    'questionType' => 'multi-choice',
+			    'wattValue' => 3,
+			    'broughtBy' => 'Genability',
+			    'answerTip' => 'Your consumption went down during business hours.',			    
+			    'answers' => array(
+			        array('answerId' => 'a','answerValue' => '0-$30', 'answerRank' => 2),
+			        array('answerId' => 'b','answerValue' => '$30-$40', 'answerRank' => 1),
+			        array('answerId' => 'c','answerValue' => '$40-$50', 'answerRank' => 3),
+			        array('answerId' => 'd','answerValue' => '$50+ ', 'answerRank' => 4)
+			        )
+			);
+
+
+
+$question3 = array(
+			    'questionId' => 3, 
 			    'questionText' => 'Which government initiative allows consumers to download their detailed energy usage?',
 			    'questionType' => 'multi-choice',
 			    'wattValue' => 1,
@@ -38,8 +73,8 @@ $question1 = array(
 			        )
 			);
 
-$question2 = array(
-			    'questionId' => 2, 
+$question4 = array(
+			    'questionId' => 4, 
 			    'questionText' => 'Consumption is measured by:',
 			    'questionType' => 'multi-choice',
 			    'wattValue' => 2,
@@ -53,8 +88,8 @@ $question2 = array(
 			        )
 			);
 
-$question3 = array(
-			    'questionId' => 3, 
+$question5 = array(
+			    'questionId' => 5, 
 			    'questionText' => 'True or False: Your cost of energy fluctuates during the day and changes at night?',
 			    'questionType' => 'multi-choice',
 			    'wattValue' => 3,
@@ -66,8 +101,8 @@ $question3 = array(
 			        )
 			);
 
-$question4 = array(
-			    'questionId' => 4, 
+$question6 = array(
+			    'questionId' => 6, 
 			    'questionText' => 'Which Utility services your location?',
 			    'questionType' => 'multi-choice',
 			    'wattValue' => 3,
@@ -82,53 +117,8 @@ $question4 = array(
 			);
 
 
-$question5 = array(
-			    'questionId' => 5, 
-			    'questionText' => 'What was your total consumption last month?',
-			    'questionType' => 'multi-choice',
-			    'wattValue' => 3,
-			    'broughtBy' => 'Genability',
-			    'answerTip' => 'Consumption rises in the winter months',			    
-			    'answers' => array(
-			        array('answerId' => 'a','answerValue' => '0-50 kWh', 'answerRank' => 1),
-			        array('answerId' => 'b','answerValue' => '50-200 kWh', 'answerRank' => 2),
-			        array('answerId' => 'c','answerValue' => '200-500 kWh', 'answerRank' => 3),
-			        array('answerId' => 'd','answerValue' => '500-1000 kWh', 'answerRank' => 4)
-			        )
-			);
-
-$question6 = array(
-			    'questionId' => 6, 
-			    'questionText' => 'What was your monthly electricity bill last month?',
-			    'questionType' => 'multi-choice',
-			    'wattValue' => 3,
-			    'broughtBy' => 'Genability',
-			    'answerTip' => 'Your consumption went down during business hours.',			    
-			    'answers' => array(
-			        array('answerId' => 'a','answerValue' => '0-50 kWh', 'answerRank' => 1),
-			        array('answerId' => 'b','answerValue' => '50-200 kWh', 'answerRank' => 2),
-			        array('answerId' => 'c','answerValue' => '200-500 kWh', 'answerRank' => 3),
-			        array('answerId' => 'd','answerValue' => '500-1000 kWh', 'answerRank' => 4)
-			        )
-			);
-
 $question7 = array(
 			    'questionId' => 7, 
-			    'questionText' => 'What was your monthly electricity bill last month?',
-			    'questionType' => 'multi-choice',
-			    'wattValue' => 3,
-			    'broughtBy' => 'Genability',
-			    'answerTip' => 'Your consumption went down during business hours.',			    
-			    'answers' => array(
-			        array('answerId' => 'a','answerValue' => '0-$30', 'answerRank' => 2),
-			        array('answerId' => 'b','answerValue' => '$30-$40', 'answerRank' => 1),
-			        array('answerId' => 'c','answerValue' => '$40-$50', 'answerRank' => 3),
-			        array('answerId' => 'd','answerValue' => '$50+ ', 'answerRank' => 4)
-			        )
-			);
-
-$question8 = array(
-			    'questionId' => 8, 
 			    'questionText' => 'True or False: Would you save money by switching to a time of use rate plan?',
 			    'questionType' => 'multi-choice',
 			    'wattValue' => 3,
@@ -140,8 +130,8 @@ $question8 = array(
 			        )
 			);			
 
-$question9 = array(
-			    'questionId' => 9, 
+$question8 = array(
+			    'questionId' => 8, 
 			    'questionText' => 'Who will win the super bowl this year?',
 			    'questionType' => 'multi-choice',
 			    'wattValue' => 3,
